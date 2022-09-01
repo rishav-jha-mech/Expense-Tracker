aa8a429a0d50d580bbf2f7248cee41c6179262b1
rishav-jha-mech <rishav.rj74@gmail.com>
2022-09-01 18:53:58 +0530

    Added gitignore models ready

--- daApp/admin.py
+++ daApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from daApp.models import Expense, Person
 
-# Register your models here.
+
+class ExpenseAdmin(admin.ModelAdmin):
+    model = Expense
+    list_display = ('date', 'amount', 'person', 'category', 'description')
+
+admin.site.register(Expense,ExpenseAdmin)
+admin.site.register(Person)
--- daApp/models.py
+++ daApp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+
+    TYPE_CHOICES = (
+        ('Me', 'Me'),
+        ('Others', 'Others'),
+    )
+    CATEGORY_CHOICES = (
+        ('Food','Food'),
+        ('Travel','Travel'),
+        ('Shopping','Shopping'),
+        ('Education','Education'),
+        ('Medical','Medical'),
+        ('Entertainment','Entertainment'),
+        ('Others','Others'),
+    )
+
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    person = models.ForeignKey(Person, on_delete=models.CASCADE,default=None,blank=True)
+    category = models.CharField(max_length=100,choices= CATEGORY_CHOICES)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
