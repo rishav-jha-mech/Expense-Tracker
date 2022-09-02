edbe86f304ad67765540ad8df6c1f3127ee4a6b4
rishav-jha-mech <rishav.rj74@gmail.com>
2022-09-02 19:53:12 +0530

    APIs ready few mods

--- daApp/urls.py
+++ daApp/urls.py
@@ -3,4 +3,8 @@ from daApp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('expense-on-a-date', views.expenseOnADate, name='expenseOnADate'),
+    path('expense-on-a-month', views.expenseOnAMonth, name='expenseOnAMonth'),
+    path('total-expense-in-a-day', views.totalExpenseInADay, name='totalExpenseInADay'),
+    path('total-expense-in-a-month', views.totalExpenseInAMonth, name='totalExpenseInAMonth'),
 ]
--- daApp/views.py
+++ daApp/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from datetime import datetime
 from daApp.models import Expense
 
 def index(request):
 
     today = datetime.now()
-    todays_expenses = Expense.objects.all()
-    todays_expenses = todays_expenses.filter(date=today.date())
+    total_expenses = Expense.objects.all()
+    todays_expenses = total_expenses.filter(date=today.date())
     monthly_expenses = Expense.objects.filter(date__month=today.month)
 
     context = {
         'expenses': todays_expenses,
         'monthly_expenses': monthly_expenses,
+        'total_expenses': total_expenses,
     }
 
     if request.method == 'POST':
@@ -46,3 +48,38 @@ def index(request):
             return redirect('index')
 
     return render(request, 'index.html', context)
+
+def expenseOnADate(request):
+    day = request.GET.get('day')
+    month = request.GET.get('month')
+    year = request.GET.get('year')
+    if year is None or month is None or day is None:
+        return JsonResponse({'data': []}, safe=False)
+    expenses = Expense.objects.filter(date__month=month, date__day=day, date__year=year).values()
+    return JsonResponse({'data': list(expenses)}, safe=False)
+
+def expenseOnAMonth(request):
+    month = request.GET.get('month')
+    year = request.GET.get('year')
+    if year is None or month is None:
+        return JsonResponse({'data': []}, safe=False)
+    expenses = Expense.objects.filter(date__month=month,date__year=year).values()
+    return JsonResponse({'data': list(expenses)}, safe=False)
+
+def totalExpenseInADay(request):
+    day = request.GET.get('day')
+    month = request.GET.get('month')
+    year = request.GET.get('year')
+    if year is None or month is None or day is None:
+        return JsonResponse({'data': 'Wrong Improper Month, Year, or Day params'}, safe=False)
+    expenses = Expense.objects.filter(date__month=month, date__day=day, date__year=year)
+    total = sum(iterable=expenses.values_list('amount', flat=True))
+    return JsonResponse({'data': total}, safe=False)
+
+def totalExpenseInAMonth(request):
+    month = request.GET.get('month')
+    year = request.GET.get('year')
+    if year is None or month is None:
+        return JsonResponse({'data': 'Wrong Improper Month or Year params'}, safe=False)
+    total = sum(iterable=expenses.values_list('amount', flat=True))
+    return JsonResponse({'data': total}, safe=False)
